7fa024f9ace12eb3a843c54f6f1a6ce2e7ef509f
David Farrugia <dfarrugia@novibet.com>
2026-08-30 22:30:22 +0200

    feat: fix log

--- esphome/components/climate_electra/electra.cpp
+++ esphome/components/climate_electra/electra.cpp
@@ -128,7 +128,9 @@ void ElectraClimate::do_transmit(bool sensor_update) {
   auto *data = transmit.get_data();
   data->set_carrier_frequency(38000);
 
-  ESP_LOGD(TAG, "AC IR payload:\n%s", this->ac->toString().c_str());
+  ESP_LOGD(TAG, "AC IR payload:");
+  ESP_LOGD(TAG, "%s", this->ac->toString().c_str());
+
   uint8_t *message = this->ac->getRaw();
 
   data->mark(kElectraAcHdrMark);
